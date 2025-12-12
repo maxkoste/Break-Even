5a5a43ef9326c1c325117bc233054827ca270322
love skön <loveskon335@gmail.com>
2025-12-12 13:15:16 +0100

    FUCK YOU

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@
 # Create endpoints that the frontend can access
 #
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
@@ -9,3 +10,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route("/")
+def get_deck():
+    url = "https://deckofcardsapi.com/api/deck/new/" 
+    response = response.get(url)
+    data = response.json()
+    print(data)
+
+get_deck()
