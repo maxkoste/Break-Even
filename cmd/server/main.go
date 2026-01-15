f53cb9f278963c1e449004fcfe5ef642e5f3dd65
Max Koste <60856309+maxkoste@users.noreply.github.com>
2026-01-15 18:07:42 +0100

    basic server written

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,28 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/api/ping", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.Write([]byte(`{"status":"ok"}`))
+	})
+
+	mux.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("web/static")),
+		),
+	)
+
+	mux.HandleFunc("/", func (w http.ResponseWriter, r *http.Request)  {
+		http.ServeFile(w,r, "web/templates/index.html")
+	})
+
+	log.Println("Server Running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
